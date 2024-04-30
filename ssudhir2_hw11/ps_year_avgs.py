9c0aaf299f6ffaba33f898c638f5bd1c68639196
Shrivats S <133294122+Stochastic1017@users.noreply.github.com>
2024-04-30 15:25:27 -0500

    re-done ssudhir2_hw11.ipynb

--- ssudhir2_hw11/ps_year_avgs.py
+++ ssudhir2_hw11/ps_year_avgs.py
@@ -2,92 +2,71 @@
 from pyspark import SparkConf, SparkContext
 import sys
 
-# this script takes two arguements, an input file, and an output directory
+# this script takes two arguements, an input file, and an output directory                                                                                      
 if len(sys.argv) != 3:
     print('Usage: ' + sys.argv[0] + ' <in> <out>')
     sys.exit(1)
 inputlocation = sys.argv[1]
 outputlocation = sys.argv[2]
 
-# Set up the configuration and job context
+# Set up the configuration and job context                                                                                                                      
 conf = SparkConf().setAppName('YearlyAverages')
 sc = SparkContext(conf = conf)
 
-# Read in the data from the input file
-data = sc.textFile(inputlocation)
-data_flat = data.flatMap(lambda line: line.split('\n'))
-data_flat = data_flat.filter(lambda x: x != '') # remove empty lines
-data_arr = data_flat.collect()
+def change_date(line):
 
-def clean_list(data_arr):
+    '''                                                                                                                                                         
+    helper function to extract relevant information from the input data                                                                                         
+                                                                                                                                                                
+    ------------ inputs ------------                                                                                                                            
+    line :  str                                                                                                                                                 
+            strings in the format 'YYYYMMDD,TMAX,TMIN'                                                                                                          
+                                                                                                                                                                
+    ------------ outputs ------------                                                                                                                           
+    result :    tuple                                                                                                                                           
+                tuples in the format (YYYY, tmax, tmin)                                                                                                         
+    '''
+
+    string_line= line.split(',')
+
+    try:
+        year = int(string_line[0][0:4])
+        tmax = int(string_line[1])
+        tmin = int(string_line[2])
+    except:
+        return None
     
-    '''
-    helper function to clean the data_arr
+    result = (year, tmax, tmin)
+    return result
 
-    ------------ inputs ------------
-    data_arr : list
-               list of strings where each string is in the format 'YYYYMMDD,TMAX,TMIN' 
-    
-    ------------ outputs ------------
-    cleaned_arr : list
-                  list of tuples where each tuple is in the format (YYYY, tmax, tmin)
-    '''
+def get_avg(line):
 
-    cleaned_arr = []
-
-    for line in data_arr:
-        curr_row = line.split(',')         # 'YYYYMMDD,TMAX,TMIN' -> ['YYYYMMDD', 'TMAX', 'TMIN']
-
-        # checking to see if every line of the file follows the format specified (and skip rows with empty values).
-        if (len(curr_row[0]) == 8) and (len(curr_row[1]) > 0) and (len(curr_row[2]) > 0):
-            year = int(curr_row[0][:4])    # 'YYYYMMDD' -> YYYY
-            tmax = float(curr_row[1])      # 'TMAX' -> tmax
-            tmin = float(curr_row[2])      # 'TMIN' -> tmin 
-            cleaned_arr.append((year, tmax, tmin)) # ['YYYYMMDD,TMAX,TMIN'] -> (YYYY, tmin, tmax)
-
-        else:
-            continue
-
-    return cleaned_arr
-
-def calculate_averages(cleaned_arr):
-
-    '''
-    helper function to calculate the average tmax and tmin for each year
-
-    ------------ inputs ------------
-    cleaned_arr : list
-                  list of tuples where each tuple is in the format (YYYY, tmax, tmin)
-
-    ------------ outputs ------------
-    averages : list
-               list of tuples where each tuple is in the format (YYYY, avgmax, avgmin)
+    '''                                                                                                                                                         
+    helper function to compute the average of the tmax and tmin for each year                                                                                   
+                                                                                                                                                                
+    ------------ inputs ------------                                                                                                                            
+    line :  str                                                                                                                                                 
+            strings in the format 'YYYYMMDD,TMAX,TMIN'                                                                                                          
+                                                                                                                                                                
+    ------------ outputs ------------                                                                                                                           
+    result :    tuple                                                                                                                                           
+                tuples in the format (YYYY, avgmax, avgmin)                                                                                                     
     '''
 
-    sums = {}
-    counts = {}
-    for year, tmax, tmin in cleaned_arr:
-        # if the year is not in the dictionary, add it
-        if year not in sums:
-            sums[year] = [tmax, tmin]
-            counts[year] = 1
+    year = line[0]
+    values = list(line[1])
+    avgmax = sum(x[1] for x in values) / len(values)
+    avgmin = sum(x[2] for x in values) / len(values)
+    result = (year, round(avgmax, 2), round(avgmin, 2))
+    return result
 
-        else:
-            sums[year][0] += tmax   # add tmax
-            sums[year][1] += tmin   # add tmin
-            counts[year] += 1       # add count
+# Read in the data from the input file                                                                                                                          
+rdd = sc.textFile(inputlocation)              # Read data from input file                                                                                       
+rdd = rdd.map(lambda line: change_date(line)) # 'YYYYMMDD,TMAX,TMIN' -> [YYYY, MM, DD, tmax, tmin]                                                              
+rdd = rdd.filter(lambda x: x != None)         # Remove empty lines                                                                                              
+rdd = rdd.groupBy(lambda x: x[0])             # Group by year                                                                                                   
+rdd = rdd.map(lambda line: get_avg(line))     # Compute the average of the tmax and tmin for each year                                                          
 
-    averages = []
-    for year in sums:
-        avgmax = round(sums[year][0]/counts[year], 2)    # take average of tmax's
-        avgmin = round(sums[year][1]/counts[year], 2)    # take average of tmin's
-        averages.append( (year, avgmax, avgmin) )        # append to the list, the tuple (year, avgmax, avgmin)
-
-    return averages
-
-# Convert the list to an RDD
-averages_rdd = sc.parallelize(calculate_averages(clean_list(data_arr)))
-averages_rdd.saveAsTextFile(outputlocation)
-
-# Let spark know the job is done, processes can be ended
+# Let spark know the job is done, processes can be ended                                                                                                        
+rdd.saveAsTextFile(outputlocation)
 sc.stop()
--- ssudhir2_hw11/ps_year_extremes.py
+++ ssudhir2_hw11/ps_year_extremes.py
@@ -13,87 +13,44 @@ outputlocation = sys.argv[2]
 conf = SparkConf().setAppName('YearlyAverages')
 sc = SparkContext(conf = conf)
 
-# Read in the data from the input file                                                                                                                         
-data = sc.textFile(inputlocation)
-data_flat = data.flatMap(lambda line: line.split('\n'))
-data_flat = data_flat.filter(lambda x: x != '') # remove empty lines                                                                                           
-data_arr = data_flat.collect()
+def change_date(line):
 
-def clean_list(data_arr):
+    '''                                                                                                                                                         
+    helper function to extract relevant information from the input data                                                                                         
+                                                                                                                                                                
+    ------------ inputs ------------                                                                                                                            
+    line :  str                                                                                                                                                 
+            strings in the format 'YYYYMMDD,TMAX,TMIN'                                                                                                          
+                                                                                                                                                                
+    ------------ outputs ------------                                                                                                                           
+    result :    tuple                                                                                                                                           
+                tuples in the format (YYYY, MM-DD, tmax, tmin)                                                                                                  
+    '''
 
-    '''                                                                                                                                                        
-    helper function to clean the data_arr                                                                                                                      
-                                                                                                                              
-    ------------ inputs ------------                                                                                                                           
-    data_arr : list                                                                                                                                            
-               list of strings where each string is in the format 'YYYYMMDD,TMAX,TMIN'                                                                         
-                                                                                                                                                               
-    ------------ outputs ------------                                                                                                                          
-    cleaned_arr : list                                                                                                                                         
-                  list of tuples where each tuple is in the format (YYYY, tmax, tmin)                                                                              '''
+    string_line = line.split(',')
 
-    cleaned_arr = []
-
-    for line in data_arr:
-        curr_row = line.split(',')         # 'YYYYMMDD,TMAX,TMIN' -> ['YYYYMMDD', 'TMAX', 'TMIN']
-
-        # checking to see if every line of the file follows the format specified (and skip rows with empty values).
-        if (len(curr_row[0]) == 8) and (len(curr_row[1]) > 0) and (len(curr_row[2]) > 0):
-            year = int(curr_row[0][:4])    # 'YYYYMMDD' -> YYYY
-            day = int(curr_row[0][4:6])    # 'YYYYMMDD' -> MM
-            month = int(curr_row[0][6:8])  # 'YYYYMMDD' -> DD                                                                                                  
-            tmax = float(curr_row[1])      # 'TMAX' -> tmax
-            tmin = float(curr_row[2])      # 'TMIN' -> tmin                                                                    
-            cleaned_arr.append((year, day, month, tmax, tmin)) # ['YYYYMMDD,TMAX,TMIN'] -> (YYYY, MM, DD, tmin, tmax)
-            
-        else:
-            continue
-
-    return cleaned_arr
-
-def find_extremes(cleaned_arr):
+    try:
+        year = int(string_line[0][0:4])
+        month = int(string_line[0][4:6])
+        day = int(string_line[0][6:])
+        tmax = int(string_line[1])
+        tmin = int(string_line[2])
+    except:
+        return None
     
-    '''
-    helper function to find the maximum tmax, and minimum tmin for each year
+    result = (year, f'{month}-{day}', tmax, tmin)
+    return result
 
-    ------------ inputs ------------
-    cleaned_arr : list
-                  list of tuples where each tuple is in the format (YYYY, tmax, tmin)
+# Read in the data from the input file                                                                                                                          
+rdd = sc.textFile(inputlocation)
+rdd = rdd.map(lambda line: change_date(line)) # 'YYYYMMDD,TMAX,TMIN' -> [YYYY, MM, DD, tmax, tmin]                                                              
+rdd = rdd.filter(lambda x: x != None)         # Remove empty lines                                                                                              
+rdd = rdd.groupBy(lambda x: x[0])             # Group by year                                                                                                   
+rdd = rdd.map(  lambda x: (x[0],                           # Year                                                                                               
+                sorted(x[1], key = lambda y: -y[2])[0][1], # Date with max value in the third column                                                            
+                sorted(x[1], key = lambda y: y[3])[0][1],  # Date with min value in the fourth column                                                           
+            ))
 
-    ------------ outputs ------------
-    extremes : list
-               list of tuples where each tuple is in the format (YYYY, MM-DD, mm-dd) 
-    '''
-
-    maximum = {}
-    minimum = {}
-    for year, day, month, tmax, tmin in cleaned_arr:
-        # if the year is not in the dictionary, add it
-        if year not in maximum:
-            maximum[year] = (tmax, f'{month}-{day}')
-        # else, if current maximum is greater than maximum before, replace it
-        else:
-            if tmax > maximum[year][0]:
-                # replace the value at key 'year' with tuple (current_maximum, MM-DD)
-                maximum[year] = (tmax, f'{month}-{day}')
-
-        # if the year is not in the dictionary, add it
-        if year not in minimum:
-            minimum[year] = (tmin, f'{month}-{day}')
-        # else, if current minimum is lesser than minimum before, replace it
-        else:
-            if tmin < minimum[year][0]:
-                # replace the value at key 'year' with tuple (current_minimum, mm-dd)
-                minimum[year] = (tmin, f'{month}-{day}')
-
-    # replace the value at key 'year' with tuple (MM-DD, mm-dd)
-    extremes = [(year, maximum[year][1], minimum[year][1]) for year in maximum]
-
-    return extremes
-
-# Convert the list to an RDD                                                                                                                                   
-extremes_rdd = sc.parallelize(find_extremes(clean_list(data_arr)))
-extremes_rdd.saveAsTextFile(outputlocation)
-
-# Let spark know the job is done, processes can be ended                                                                                                      
+# Let spark know the job is done, processes can be ended                                                                                                        
+rdd.saveAsTextFile(outputlocation)
 sc.stop()
